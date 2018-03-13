b3e3ed1e9d0c21befa0ece90aacbdf8022a2da2b
Unknown <bcrussi@gmail.com>
2018-03-13 15:46:43 -0300

    project structure

---
+++ src/app/app.module.ts
@@ -0,0 +1,62 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { NgModule, ErrorHandler } from '@angular/core';
+
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { IonicStorageModule } from '@ionic/storage';
+
+import { ConferenceApp } from './app.component';
+import { AboutPage } from '../pages/about/about';
+import { SchedulePage } from '../pages/schedule/schedule';
+import { SessionDetailPage } from '../pages/session-detail/session-detail';
+import { TabsPage } from '../pages/tabs-page/tabs-page';
+import { TutorialPage } from '../pages/tutorial/tutorial';
+import { ConferenceData } from '../providers/conference-data';
+import { UserData } from '../providers/user-data';
+
+
+@NgModule({
+  declarations: [
+    ConferenceApp,
+    AboutPage,
+    SchedulePage,
+    SessionDetailPage,
+    TabsPage,
+    TutorialPage,
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    IonicModule.forRoot(ConferenceApp, {}, {
+      links: [
+        { component: TabsPage, name: 'TabsPage', segment: 'tabs-page' },
+        { component: SchedulePage, name: 'Schedule', segment: 'schedule' },
+        { component: SessionDetailPage, name: 'SessionDetail', segment: 'sessionDetail/:sessionId' },
+        { component: AboutPage, name: 'About', segment: 'about' },
+        { component: TutorialPage, name: 'Tutorial', segment: 'tutorial' },
+      ]
+    }),
+    IonicStorageModule.forRoot()
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    ConferenceApp,
+    AboutPage,
+    SchedulePage,
+    SessionDetailPage,
+    TabsPage,
+    TutorialPage,
+  ],
+  providers: [
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    ConferenceData,
+    UserData,
+    InAppBrowser,
+    SplashScreen
+  ]
+})
+export class AppModule { }
